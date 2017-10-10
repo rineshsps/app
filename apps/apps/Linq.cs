467434eec3bda36b75c26ba2ae84fd2b2b64e630
Rinesh Kanamadathil <v-rinkan@microsoft.com>
2017-10-10 12:02:32 +0530

    Your commit message

---
+++ apps/apps/Linq.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apps
+{
+    class Linq
+    {
+        public Linq()
+        {
+            decimal[] numbers = { 101, 110, 55, 32, 300, 20 };
+
+            //selecting number > 100
+            dynamic result = numbers.Where(x => x > 100);
+            
+            // or 
+            result = numbers.Where(x => x > 100)
+                            .Select(x => x);
+
+            //order by desc
+            result = numbers
+                            .OrderByDescending(x => x);
+        }
+
+    }
+}
---
+++ apps/apps/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Linq ln = new Linq();
+
+            string texts = "AB01-";
+            char[] text = texts.ToArray();
+            foreach (char item in text)
+            {
+                bool isNumber = char.IsNumber(item);
+                var isLetter = char.IsLetter(item);
+                if (isNumber)
+                {
+                    Console.WriteLine("num");
+                }
+                else if (isLetter)
+                    Console.WriteLine("letter");
+                else
+                    Console.WriteLine(item.GetType());
+            }
+        }
+    }
+}
